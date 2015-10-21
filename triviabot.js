4c073d7abbccca9d7e2ef0187d26d3f28ef7625c
Sam Watson <sam@toomuch.com.au>
2015-10-21 23:10:44 +0800

    Added logging of error message JSON to try to understand error format.

--- triviabot.js
+++ triviabot.js
@@ -1727,6 +1727,10 @@ function run_bot(cookie) {
         console.log('caught error:', err);
 		// console.log('logging in again');
 		// login_then_run_bot();
+		
+		// ### DISCONNECTED ###
+		// caught error:   can not find session
+		console.log(JSON.stringify(err));
     });
 
     socket.on('init', function(data) {
